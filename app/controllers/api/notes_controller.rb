5ec552c859366839a75e7a101c8acdfc3231221f
Wenyuan Wu <wenyuan.wu2012@gmail.com>
2017-07-20 12:05:52 -0700

    note controller

--- app/controllers/api/notes_controller.rb
+++ app/controllers/api/notes_controller.rb
@@ -1,6 +1,45 @@
 class Api::NotesController < ApplicationController
 	
-		
+	def create 
+		@note = Note.new(note_params)
+
+		if@note.save 
+			render "api/notes/show"
+		else 
+			render(
+				json: @note.errors.full_messages, 
+				status: 422)
+		end 
+	end 
+
+	def edit 
+		@note = Note.find(params[:id])
+	end 
+
+	def update 
+		@note = Note.find(params[:id])
+		@note.update_attributes(note_params)
+		render "api/notes/show"
+	end 
+
+	def destroy 
+		@note = Note.find(params[:id])
+		@note.destroy 
+		render "api/notes/show" 
+		# what's the return value 
+
+	end
+
+	def show 
+		@note = Note.find(params[:id])
+		render "api/notes/show"
+		# do we need to render 
+	end 	
+
+	def index 
+		@note = Note.find_notes_by_user(current_user)
+		render "api/notes/index"
+	end 
 
 	private
 
