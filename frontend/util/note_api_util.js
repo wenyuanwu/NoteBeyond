725d8d131c69aa25eb22d819b84463a50cbf30cc
Wenyuan Wu <wenyuan.wu2012@gmail.com>
2017-07-20 17:38:02 -0700

    note api util

---
+++ frontend/util/note_api_util.js
@@ -0,0 +1,37 @@
+
+export const createNote = note => (
+  $.ajax({
+    method: 'POST',
+    url: '/api/notes',
+    data: note
+  })
+);
+
+export const updateNote = note => (
+  $.ajax({
+    method: 'PATCH',
+    url: `/api/notes/${note.id}`,
+    data: note
+  })
+);
+
+export const fetchSingleNote = id => (
+  $.ajax({
+    method: 'GET',
+    url: `/api/notes/${id}`
+  })
+);
+
+export const fetchAllNotes = () => (
+  $.ajax({
+    method: 'GET',
+    url: '/api/notes'
+  })
+);
+
+export const deleteNote = id => (
+  $.ajax({
+    method: 'DELETE',
+    url: `/api/notes/${id}`
+  })
+);
