203c6997ccbdc3609bdb3a7c76cb664733372a5c
Wenyuan Wu <wenyuan.wu2012@gmail.com>
2017-07-26 10:05:17 -0700

    rebuild

--- frontend/components/note_delete/note_delete.jsx
+++ frontend/components/note_delete/note_delete.jsx
@@ -17,11 +17,15 @@ class NoteDelete extends React.Component {
 	render(){
 		let {currentNote} = this.props;
 
+		if(! currentNote){
+			return <h3 className="loading"> Loading...</h3>;
+		}
+
 		return(
 			<div className="deleteNotePage">
 				<img className="delete-button-delete-page" src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAACgAAAAjCAYAAADmOUiuAAACbElEQVR42u2XP2gTURzHg5aKQ5dSig5OtlgcstgidnAyWC2YRVPEwUVS0cGpRlwylVQcHEohggGH5nJ3uaQd5CCBgmJTSEDQoYiBDkFMIYKgg0iVxt+Tb8ur3jtyd+8Fhxw8km/uvd/v877vb0Kh3tN7/n3y+XyUyjaVdoeF1Y12E9AL3D5kNwH/JFVVvwfYFUAKWPExz/yWyv8O+DqQm6Zp3kOgJ0FHxrKsRRaLPu9KG27DMG4C8LmEqbOMWDekAZKDVxB0lduo2/T7ig/AF3DwskwHzwPoFdOFQmFPv/QxxOvo7KQ0QE3Twgj6DknCcOGtDwc30bnT0gBzudwJADactEfAT6wtjcJxaYCZTGYAQN/+0l99AH6Hg0dlb9w/qewmk8k+XlOiw53GsG37CDr2Q/rxRiCfWXCaj0MAbPG6wxjHlN1sKGidBc9ms6NumiBGXFbwGADfq3CwxoLruj4BoKqbFsQ4B8ANFYBlFrxYLEYAVHLSVC/iMgqXAGirADSx98UwXDqv6bvBNG3qMZchvg5ATQXgUwDFcbqkkWwWgGn+vcDBO2izJB2QgB4heALAKV4T2AEtAHyIOvMqHHwAh1JIdh/JFtCBBK8FgI8xT+ekAxLYbQCkARx304JOPkOdW9IB6eycgYM63LiGZIaTFgBacPCqCgcvAqCEZBEkK/N6770AcA0r/YIKwLMAqEKPA7DGNNugeS0AfIM6Z1QsklMY4g/QIwCuO2nBItnCaXNSOiCdGMMAaAXo5Bc4OKjCwX4A7vhpT9e0Q9T2l9crmtcbzUcJ/4EbIVUP9XyKEjQDwDUpxrSXnL8BI9Tv3UAALyAAAAAASUVORK5CYII=" />
 				<div className="lineOne">DELETE NOTE</div>
-				<div className="lineTwo">Are you sure you want to delete {currentNote.title}</div>
+				<div className="lineTwo">Are you sure you want to delete <span className="noteName">{currentNote.title}</span> ?</div>
 				<Link to={"/"}>Cancel</Link>
 				<button onClick={this.handleDelete}>Delete</button>
 			</ div>
