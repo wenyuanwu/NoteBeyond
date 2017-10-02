6e1338f6ab6363519bb94521b17faa4f839c0c34
Wenyuan Wu <wenyuan.wu2012@gmail.com>
2017-10-02 16:53:36 -0700

    drop down list

--- frontend/components/note_dropdown_list/note_dropdown_list.jsx
+++ frontend/components/note_dropdown_list/note_dropdown_list.jsx
@@ -4,6 +4,9 @@ class NoteDropDownList extends React.Component {
 	constructor(props){
 		super(props);
 		this.state = {listStatus: false};
+		this.toggleListStatus = this.toggleListStatus.bind(this);
+		this.isCurrentNotebook = this.isCurrentNotebook.bind(this);
+		this.hideList = this.hideList.bind(this);
 	}
 
 	componentDidMount(){
@@ -14,11 +17,20 @@ class NoteDropDownList extends React.Component {
 		this.setState({listStatus: !this.state.listStatus});
 	}
 
+	hideList(){
+		this.setState({listStatus: false});
+	}
+
+	isCurrentNotebook(notebook){
+		return notebook.id === this.props.currentNotebook.id;
+	}
+
+
 	render(){
-		let {notebook, currentNotebook} = this.props;
-		let className = 'hidden';
+		let {notebooks, currentNotebook} = this.props;
+		let listName = 'hidden';
 		if (this.state.listStatus){
-			className = 'drop-down-list-show';
+			listName = 'drop-down-list-show';
 		}
 
 		let Title = "";
@@ -31,7 +43,27 @@ class NoteDropDownList extends React.Component {
 				<button className='notebook-drop-down-list-btn' onClick = {this.toggleListStatus}>
 					<img className="selector-icon" src="http://res.cloudinary.com/dltydzsmu/image/upload/v1506467913/notebook_obz1qs.png"/>
 					<span className="notebook-drop-down-list-title">{Title}</span>
-				</button>	
+				</button>
+				<ul className = {listName}>
+					<li key = {0} className = "notebook-list-header">
+						<div> notebook </div>
+					</li>
+					{notebooks.map(notebook => {
+						let liClass = "notebook-item";
+						if(this.isCurrentNotebook(notebook)) {
+							liClass += "current";
+						}
+						return(
+							<li className = {liClass}
+								key = {notebook.id}
+								onClick = {this.hideList}>
+								<div>{notebook.title}</div>
+							</li>
+						);
+					})}
+					
+
+				</ul>	
 			</div>	
 
 		);
